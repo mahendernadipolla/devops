610b9a0d67e4742efab1744d8dda40c3e8ab817e
mahender <mahender.nani1994@gmail.com>
2024-10-17 16:33:07 +0530

    ia

---
+++ src/test/java/mahenderdevops/mahenderdevops/mahiclass.java
@@ -0,0 +1,23 @@
+package mahenderdevops.mahenderdevops;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class mahiclass {
+	@Test
+	void start() {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mahender\\Automation\\mahenderdevops\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://automationtalks.com/");
+		driver.close();
+		
+		
+	}
+
+}
